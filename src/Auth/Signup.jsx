ce65c4fb0a60a66a8fa5e469d4ca8b0185ec8fba
Muhammadkashif20 <unnamed9080@gmail.com>
2025-10-30 23:19:42 +0500

    working on signup page

--- src/Auth/Signup.jsx
+++ src/Auth/Signup.jsx
@@ -66,23 +66,24 @@ const SignUp = () => {
             </div>
 
             {/* Role Selection */}
-            <div>
-              <label className="block text-gray-700 mb-1 text-sm font-medium">
-                Select Role
-              </label>
-              <select
-                name="role"
-                className="w-full px-3 py-2 rounded-md border border-gray-300 bg-white text-gray-700 text-sm focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 outline-none"
-                defaultValue=""
-              >
-                <option value="" disabled>
-                  Choose your role
-                </option>
-                <option value="student">Student</option>
-                <option value="teacher">Teacher</option>
-                <option value="admin">Admin</option>
-              </select>
-            </div>
+         <div>
+  <label className="block text-gray-700 mb-1 text-sm font-medium">
+    Select Role
+  </label>
+  <select
+    name="role"
+                   className="w-full px-3 py-2 rounded-md border border-gray-300 focus:ring-2 focus:ring-indigo-500  outline-none text-sm"
+
+    defaultValue=""
+  >
+    <option value="" disabled>
+      Choose your role
+    </option>
+    <option value="student">Student</option>
+    <option value="teacher">Teacher</option>
+    <option value="admin">Admin</option>
+  </select>
+</div>
 
             {/* Submit Button */}
             <button
